cf3951bf54b3cab07c8e51abdcf353ae613f1595
Aranthis1 <maximilian.jaracid@gmail.com>
2026-01-20 15:00:55 -0300

    2.0

---
+++ explorador_django/README.md
@@ -0,0 +1,5 @@
+## 📅 Actualización: [20/01/2025]
+
+### 🛠️ Cambios Técnicos / Refactorización
+- Actualización de dependencias.
+- Mejora en la estructura de archivos de `explorador_django`.
--- explorador_django/explorador_django/explorador_django/settings.py
+++ explorador_django/explorador_django/explorador_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m!jg_2k_+*l)qlopf57j!-einv2@p%!mnj&ay(i@&_vx_vnp)&'
+SECRET_KEY = 'django-insecure-en$n%#wg4#ypsmvyxqrvi#25^3_9jok+sw7ao5p7nlo%1zw#60'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'configuracion',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'explorador_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
